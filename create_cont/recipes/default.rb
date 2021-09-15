7f3a4d160c250f631c93a64ebe2746478c8e62ee
16-t-2146-k <16t2146k@shinshu-u.ac.jp>
2021-09-16 06:49:29 +0900

    provisioning_nodeとcreate_contを変更

--- create_cont/recipes/default.rb
+++ create_cont/recipes/default.rb
@@ -1,4 +1,33 @@
-search(:classes, "uid:20w2081d").each do |result|
+server_data = data_bag_item('server', node[:hostname])
+
+search(:classes, "uid:#{node[:hostname]}").each do |result|
+
+    ruby_block "set port #{result['cid']}-#{result['uid']} databag items" do
+        block do
+            if result['port'].empty? then
+                _port = server_data['port'].shift
+                server_data['used_port'].push(_port)
+                result['port'] = _port
+
+                _ip = server_data['ip'].shift
+                server_data['used_ip'].push(_ip)
+                result['ip'] = _port                
+
+                databag_item = Chef::DataBagItem.new
+                databag_item.data_bag('classes')
+                databag_item.raw_data = {
+                    'id' => result['id'],
+                    'uid' => result['uid'],
+                    'cid' => result['cid'],
+                    'ip' => result['ip']
+                    'port' => result['port'],
+                }
+                databag_item.save
+
+            end
+        end
+    end
+
     ruby_block "check container #{result['cid']}-#{result['uid']}" do
         block do
             cont = `/snap/bin/lxc list --format json | jq '.[] | select(.name == \"#{result['cid']}-#{result['uid']}\") | {user:(.name),dist:.state.network.eth0.addresses[] | select(.family == \"inet\") | .address}'`
@@ -8,24 +37,105 @@ search(:classes, "uid:20w2081d").each do |result|
             end
         end
         if `/snap/bin/lxc list --format json | jq '.[] | select(.name == \"#{result['cid']}-#{result['uid']}\") | {user:(.name),dist:.state.network.eth0.addresses[] | select(.family == \"inet\") | .address}'` == '' then
-            notifies :run, "bash[create env #{result['cid']}-#{result['uid']}]", :immediately
+            notifies :run, "bash[lxc init #{result['cid']}-#{result['uid']}]", :immediately
         end
     end
 
-    bash "create env #{result['cid']}-#{result['uid']}" do
-        user 'vagrant'
+    bash "lxc init #{result['cid']}-#{result['uid']}" do
+        user 'ubuntu'
         group 'lxd'
-        cwd '/home/vagrant'
+        cwd '/home/ubuntu'
         action :nothing
-        notifies :run, "bash[create device #{result['cid']}-#{result['uid']}]", :immediately
-        code "/snap/bin/lxc launch chefserver:#{result['cid']} #{result['cid']}-#{result['uid']}"
+        notifies :run, "bash[lxc config device add #{result['cid']}-#{result['uid']} http proxy]", :immediately
+        code "/snap/bin/lxc init chefserver:#{result['cid']} #{result['cid']}-#{result['uid']}"
     end
 
-    bash "create device #{result['cid']}-#{result['uid']}" do
-        user 'vagrant'
+    bash "lxc config device add #{result['cid']}-#{result['uid']} http proxy" do
+        user 'ubuntu'
         group 'lxd'
-        cwd '/home/vagrant'
+        cwd '/home/ubuntu'
         action :nothing
-        code "/snap/bin/lxc config device add #{result['cid']}-#{result['uid']} http proxy listen=tcp:0.0.0.0:#{result['port']} connect=tcp:$(/snap/bin/lxc list --format json | jq -r '.[] | select(.name == \"#{result['uid']}\") | .state.network.eth0.addresses[] | select(.family == \"inet\") | .address'):80 bind=host"
+        notifies :run, "bash[lxc network attach lxdbr0 #{result['cid']}-#{result['uid']} eth1]", :immediately
+        code "/snap/bin/lxc config device add #{result['cid']}-#{result['uid']} http proxy listen=tcp:0.0.0.0:#{result['port']} connect=tcp:#{result['ip']}:80 bind=host"
+    end
+
+    bash "lxc network attach lxdbr0 #{result['cid']}-#{result['uid']} eth1" do
+        user 'ubuntu'
+        group 'lxd'
+        cwd '/home/ubuntu'
+        action :nothing
+        notifies :run, "bash[lxc config device set #{result['cid']}-#{result['uid']} eth1 ipv4.address #{result['ip']}]", :immediately
+        code "/snap/bin/lxc network attach lxdbr0 #{result['cid']}-#{result['uid']} eth1"
+    end
+
+    bash "lxc config device set #{result['cid']}-#{result['uid']} eth1 ipv4.address #{result['ip']}" do
+        user 'ubuntu'
+        group 'lxd'
+        cwd '/home/ubuntu'
+        action :nothing
+        notifies :run, "bash[lxc start #{result['cid']}-#{result['uid']}]", :immediately
+        code "/snap/bin/lxc config device set #{result['cid']}-#{result['uid']} eth1 ipv4.address #{result['ip']}"
+    end
+
+    bash "lxc start #{result['cid']}-#{result['uid']}" do
+        user 'ubuntu'
+        group 'lxd'
+        cwd '/home/ubuntu'
+        action :nothing
+        code "/snap/bin/lxc start #{result['cid']}-#{result['uid']}"
+    end
+
+    directory '/home/ubuntu/rasapp/public/classes' do
+        owner 'ubuntu'
+        group 'ubuntu'
+        mode '0755'
+        action :create
+    end
+
+    directory '/home/ubuntu/rasapp/public/classes/contents' do
+        owner 'ubuntu'
+        group 'ubuntu'
+        mode '0755'
+        action :create
+    end
+
+    directory '/home/ubuntu/rasapp/public/classes/images' do
+        owner 'ubuntu'
+        group 'ubuntu'
+        mode '0755'
+        action :create
+    end
+
+    file "/home/ubuntu/rasapp/public/classes/contents/#{result['cid']}.json" do
+        content "{\"ip\":#{result['ip']},\"port\":\"#{result['port']}\"}"
+        mode '0755'
+        owner 'ubuntu'
+        group 'ubuntu'
+        action :create
+    end
+
+    remote_file "/home/ubuntu/rasapp/public/classes/images/#{result['cid']}.png" do
+        source "file:///home/vagrant/images/#{result['cid']}.png"
+        owner 'ubuntu'
+        group 'ubuntu'
+        mode '0755'
+        action :create
+    end
+end
+
+ruby_block "set #{result['cid']} databag items" do
+    block do
+        databag_item = Chef::DataBagItem.new
+        databag_item.data_bag('server')
+        databag_item.raw_data = {
+            'id' => server_data['id'],
+            'uid' => server_data['uid'],
+            'cid' => server_data['cid'],
+            'ip' => server_data['ip'],
+            'used_ip' => server_data['used_ip'],
+            'port' => server_data['port'],
+            'used_port' => server_data['used_port'],
+        }
+        databag_item.save
     end
 end
--- provisioning_node/recipes/default.rb
+++ provisioning_node/recipes/default.rb
@@ -8,16 +8,6 @@ apt_package 'lxc' do
     not_if 'which lxc'
 end
 
-bash 'snap install lxd' do
-    user 'ubuntu'
-    group 'root'
-    action  :run
-    cwd '/home/ubuntu'
-    code "sudo snap install lxd"
-    not_if { File.exist?("/snap/bin/lxd") }
-    notifies :run, 'bash[lxd init]', :delayed
-end
-
 apt_package 'jq' do
     action :install
     options '-y'
@@ -37,13 +27,33 @@ template "/tmp/chef/lxd_init.txt" do
     action :create
 end
 
+template "/etc/netplan/70-eth1.yaml" do
+    source "70-eth1.erb"
+    action :create
+end
+
+template "/home/ubuntu/mydefault.yaml" do
+    source "mydefault.erb"
+    action :create
+end
+
+bash 'snap install lxd' do
+    user 'ubuntu'
+    group 'root'
+    action  :run
+    cwd '/home/ubuntu'
+    code "sudo snap install lxd"
+    not_if { File.exist?("/snap/bin/lxd") }
+    notifies :run, 'bash[lxd init]', :immediately
+end
+
 bash "lxd init" do
     user 'ubuntu'
     group 'lxd'
     cwd '/home/ubuntu'
     action :nothing
     code "/snap/bin/lxd init < /tmp/chef/lxd_init.txt"
-    notifies :run, 'bash[lxd remote add]', :delayed
+    notifies :run, 'bash[lxd remote add]', :immediately
 end
 
 bash "lxd remote add" do
@@ -52,4 +62,13 @@ bash "lxd remote add" do
     cwd '/home/ubuntu'
     action :nothing
     code "/snap/bin/lxc remote add chefserver https://chefserver:8443 --accept-certificate --password securitysecur"
+    notifies :run, 'bash[lxc profile edit default]', :immediately
+end
+
+bash "lxc profile edit default" do
+    user 'ubuntu'
+    group 'lxd'
+    cwd '/home/ubuntu'
+    action :nothing
+    code "cat mydefault.yaml | /snap/bin/lxc profile edit default"
 end
