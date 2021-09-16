ae60148d787ecfc37e3f0db957c25918793e11a2
16-t-2146-k <16t2146k@shinshu-u.ac.jp>
2021-09-17 02:32:09 +0900

    change create_cont

--- create_cont/recipes/default.rb
+++ create_cont/recipes/default.rb
@@ -26,7 +26,7 @@ search(:classes, "uid:#{node[:hostname]}").each do |result|
 
             end
         end
-        notifies :run, "ruby_block [set #{server_data['hostname']} databag items]", :delayed
+        notifies :run, "ruby_block[set #{server_data['hostname']} databag items]", :delayed
     end
 
     ruby_block "check container #{result['cid']}-#{result['uid']}" do
