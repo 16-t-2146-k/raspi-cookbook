ea123ffda3f9e5d1999ff70b8fb273c2d1de7ee9
16-t-2146-k <16t2146k@shinshu-u.ac.jp>
2021-11-10 05:44:10 +0900

    databagを削除するレシピを追加

--- delete_databag/recipes/default.rb
+++ delete_databag/recipes/default.rb
@@ -5,8 +5,8 @@ ruby_block "delete classes databag items" do
     block do
         #override_attributes
         Chef::Log.info node['delete_databag']['item']
-        databag_item = Chef::DataBagItem.new
-        databag_item.load('classes',node['delete_databag']['item'])
+        databag_item = data_bag_item('classes', node['delete_databag']['item'])
+        #databag_item.load('classes',node['delete_databag']['item'])
         databag_item.delete()
     end
     action :run
